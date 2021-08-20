6e193c970e3796f4bbef581a58b2cddfd20067d3
gajovich <75524971+gajovich@users.noreply.github.com>
2021-08-20 22:22:40 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-const swiper = new Swiper('.swiper-real', {
+new Swiper('.swiper-real', {
    direction: 'horizontal',
    loop: true,
    speed: 1000,
+   spaceBetween: 15,
    pagination: {
       el: '.swiper-real__dots',
       clickable: true,
@@ -11,4 +12,17 @@ const swiper = new Swiper('.swiper-real', {
       nextEl: '.swiper-real__arrow-next',
       prevEl: '.swiper-real__arrow-prev',
    },
+});
+
+new Swiper('.client__swiper', {
+   direction: 'horizontal',
+   loop: true,
+   speed: 1000,
+   spaceBetween: 15,
+   pagination: {
+      el: '.client__dots',
+      clickable: true,
+   },
+
+  
 });
